8ea1ade43b752173a6efe5d07b375b8532587a21
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-14 15:50:46 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -7,7 +7,7 @@ class GetRequester
   def initialize(url)
     @url = url
   end 
-  def get_response_body(url)
+  def get_response_body
   url =  https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json
   uri = URI.parse(url)
   response = Net::HTTP.get_response_body(uri)
