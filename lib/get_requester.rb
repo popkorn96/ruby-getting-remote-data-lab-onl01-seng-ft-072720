5cdd04438648f611571f497c099deb8b9be95933
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-14 15:40:01 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,9 +1,16 @@
-
+require 'open-uri'
+require 
 
 
 class GetRequester
-  def initialization 
-    
+  def initialize
+    url = 
+  end
+  def get_response_body
+  uri = URI.parse(URL)
+  response = Net::HTTP.get_response(uri)
+  response.body
+  end
 end
   
   
